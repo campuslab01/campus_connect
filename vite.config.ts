17874864482103c046af0b3d34e54a22cda28b04
campuslab01 <campuslab01>
2025-10-20 00:40:43 +0530

    fix mimes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  define: {
+    'process.env.VITE_BASE_PATH': JSON.stringify(mode === 'production' ? '/' : '/campus_connect/'),
+  },
   base: mode === 'production' ? '/' : '/campus_connect/',
   build: {
     chunkSizeWarningLimit: 1600,
