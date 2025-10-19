6a6501c3b92687e8d661587664bdee8ce4ac0bc2
campuslab01 <campuslab01>
2025-10-20 00:32:35 +0530

    commit new

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command, mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/campus_connect/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/campus_connect/',
   build: {
-    chunkSizeWarningLimit: 2000, // KB
+    chunkSizeWarningLimit: 1600,
     rollupOptions: {
       output: {
         manualChunks(id) {
