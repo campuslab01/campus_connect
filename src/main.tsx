1f91fcdd961f100e7ead85cb0107f300c094e526
campuslab01 <campuslab01>
2025-10-28 02:13:30 +0530

    remove router basename

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.PROD ? '/campus_connect/' : '/'}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
